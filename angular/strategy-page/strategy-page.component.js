7c45ee6cbc4694b8c6a108f19e1253d3a2bf935e
baltasarsolanilla <solanilla.baltasar@gmail.com>
2018-04-02 19:27:40 -0300

    Arreglos en strategy-page

--- angular/strategy-page/strategy-page.component.js
+++ angular/strategy-page/strategy-page.component.js
@@ -5,5 +5,11 @@ angular.
     component('strategyPage', {
         templateUrl: '../angular/strategy-page/strategy-page.html',
         controller: function ResumenTableObjetivosController($scope){
+
+            $scope.onSelectTreeItem = $scope.onSelectTreeItem;
+            
+            function onSelectTreeItem(item){
+                $scope.treeItemSelected = item;
+            } 
         }
     });
