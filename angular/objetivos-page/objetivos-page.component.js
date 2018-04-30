20b7c2ad90243484b2242a304d7902abbbad1430
baltasarsolanilla <solanilla.baltasar@gmail.com>
2018-04-30 09:31:25 -0300

    no me acuerdo

--- angular/objetivos-page/objetivos-page.component.js
+++ angular/objetivos-page/objetivos-page.component.js
@@ -18,22 +18,22 @@ angular.
 
 
                 function onSelectObjetivo(value){
-                    $window.$window.console.log(controllerName + "onSelectObjetivo(value)");
+                    $window.console.log(controllerName + "onSelectObjetivo(value)");
                     $scope.selectedObjetivo = value;
                 }
 
                 function createObjetivo(){
-                    $window.$window.console.log(controllerName + "createObjetivo()");
+                    $window.console.log(controllerName + "createObjetivo()");
                     $window.alert("CREAR OBJETIVO");
                 }
 
                 function updateObjetivo(){
-                    $window.$window.console.log(controllerName + "updateObjetivo()");
+                    $window.console.log(controllerName + "updateObjetivo()");
                     $window.alert("UPDATE OBJETVIO " + $scope.selectedObjetivo.name);
                 }
 
                 function deleteObjetivo(){
-                    $window.$window.console.log(controllerName + "deleteObjetivo()");
+                    $window.console.log(controllerName + "deleteObjetivo()");
                     $window.alert("ELIMINAR OBJETIVO: " + $scope.selectedObjetivo.name);
                 }
             }
