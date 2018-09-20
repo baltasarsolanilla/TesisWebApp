267d68274176c301111317b52095894c24b9d2a4
baltasar <bsolanilla@technisys.com>
2018-09-20 12:34:31 -0300

    Eliminacion de sobras - version final DONE

--- angular/indicadores-page/indicadores-page.component.js
+++ angular/indicadores-page/indicadores-page.component.js
@@ -4,45 +4,11 @@ angular.
     module('indicadoresPage').
         component('indicadoresPage', {
             templateUrl: '../angular/indicadores-page/indicadores-page.html',
-            controller: function ObjetivosPageController($scope, $window, $uibModal, Indicador){
+            controller: function ObjetivosPageController($scope, Indicador){
                 var controllerName = "INDICADOR-PAGE-CONTROLLER -> ";  
                
-                $scope.createIndicador = createIndicador;
-                $scope.updateIndicador = updateIndicador;
-                $scope.deleteIndicador = deleteIndicador;
-                $scope.onSelectIndicador = onSelectIndicador;
-                $scope.selectedIndicador = null;
-
                 this.$onInit = function(){
                     cargarIndicadores();
-                }
-
-                function onSelectIndicador(indicador){
-                    $scope.selectedIndicador = indicador;
-
-                }
-
-                function updateIndicador(){
-                    $window.alert("Modificar indicador con id: " + $scope.selectedIndicador.nombre);
-                }
-
-                function deleteIndicador(){
-                    $window.alert("Eliminar indicador con id: " + $scope.selectedIndicador.nombre);
-                }
-
-                function createIndicador(){
-                    var modalInstance = $uibModal.open({
-                      animation: true,
-                      component: 'modalComponentIndicador'
-                    });
-
-                    modalInstance.result.then(function (indicador) {
-                      Indicador.save(indicador, function(){
-                          
-                      })
-                    }, function () {
-                      $window.console.log('modal-component dismissed at: ' + new Date());
-                    });
                 }
 
                 //AJAX
@@ -51,39 +17,7 @@ angular.
                       delete indicadores.$promise;
                       delete indicadores.$resolved;
                       $scope.indicadores = indicadores;
-                      console.log($scope.indicadores);
                     });
-                  }
+                }
             }
         });
-angular.
-    module('indicadoresPage').
-        component('modalComponentIndicador', {
-            templateUrl: '../angular/indicadores-page/indicadores-page-modals/crear-indicador.modal.html',
-            bindings: {
-                close: '&',
-                dismiss: '&'
-            },
-            controller: function ($window) {
-                var $ctrl = this;
-                var controllerName = "INDICADOR-PAGE-MODAL -> ";
-
-                $ctrl.$onInit = function () {
-
-                };
-
-                $ctrl.indicadorForm = {
-                    nombre: "",
-                    valor: ""
-                };
-
-                $ctrl.ok = function () {
-                $window.console.log(controllerName + "ok()");
-                $ctrl.close({$value: $ctrl.indicadorForm});
-                };
-
-                $ctrl.cancel = function () {
-                $ctrl.dismiss({$value: 'cancel'});
-                };
-            }
-    });
--- angular/indicadores-page/kpi-all-table/kpi-all-table.component.js
+++ angular/indicadores-page/kpi-all-table/kpi-all-table.component.js
@@ -6,7 +6,6 @@ angular.
         templateUrl: '../angular/indicadores-page/kpi-all-table/kpi-all-table.html',
         bindings: {
           data: '<',
-          onSelect: '&'
         },
         controller: function ResumenTableObjetivosController($scope, NgTableParams, BuilderTable){
             var controllerName = "KPI-ALL-TABLE-CONTROLLER -> ";
@@ -15,11 +14,9 @@ angular.
               id: 1,
               nombre: 'Ganarle a Juanca',
               valor: 5.5
-              //tendencia:
             }];
 
             this.$onInit = function() {
-              fillRandomTendencia(kpi_data);
               $scope.tableParams = new NgTableParams({
                 page: 1, // show first page
                 count: 10 // count per page
@@ -36,40 +33,15 @@ angular.
             };
 
             function changeDataTable(data){
-              fillRandomTendencia(data);
               $scope.tableParams.settings({
                 dataset: angular.copy(data)
               });
               $scope.tableParams.reload();
             }
 
-            function fillRandomTendencia(array){
-              array.forEach(element => {
-                element.tendencia = BuilderTable.getRandomTendencia();
-              });
-            }
-
             // set color valor
             $scope.setColorValor = function(valor){
               return BuilderTable.setColorValor(valor);
-            }
-            
-            // set color tendencia
-            $scope.setColorTendencia = function(tendencia){
-              return BuilderTable.setColorTendencia(tendencia);
-            }
-    
-            // class="fas fa-arrow-circle-up"
-            $scope.setArrowTendencia = function(tendencia){
-              return BuilderTable.setArrowTendencia(tendencia);
-            }
-            
-            $scope.onSelectIndicador = onSelectIndicador;
-            $scope.selectedIndicador = null;
-
-            function onSelectIndicador(value){
-              $scope.selectedIndicador = value;
-              $scope.$ctrl.onSelect({indicador: $scope.selectedIndicador});
             }
         }
     });
